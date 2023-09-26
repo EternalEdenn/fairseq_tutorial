2126b4771d601ca73074a3576ff901e75ee64269
2A <102230919+EternalEdenn@users.noreply.github.com>
2023-09-26 17:47:31 +0900

    Create 01.dataprocess.sh

---
+++ scripts/01.dataprocess.sh
@@ -0,0 +1,16 @@
+DIR=../data
+
+mkdir $DIR/data-bin/
+
+fairseq-preprocess \
+    --source-lang en \
+    --target-lang ja \
+    --trainpref $DIR/aspec_train_h100k.tkn\
+    --validpref $DIR/aspec_dev.tkn \
+    --testpref $DIR/aspec_test.tkn \
+    --bpe subword_nmt \
+    --destdir $DIR/data-bin \
+    --thresholdtgt 0 \
+    --thresholdsrc 0 \
+    --task translation \
+    --workers 70
