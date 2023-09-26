1ac69b814beeacd778b9b85cd1c9438b055f9eaa
2A <102230919+EternalEdenn@users.noreply.github.com>
2023-09-26 17:48:33 +0900

    Create 03.prediction.sh

---
+++ scripts/03.prediction.sh
@@ -0,0 +1,29 @@
+export OMP_WAIT_POLICY=PASSIVE
+
+DIR=..
+DATAPATH=../data/data-bin
+
+
+mkdir $DIR/fairseq_out_test/
+
+
+BASE_MODEL=$DIR/fairseq_trained-model/checkpoint_best.pt
+fairseq-generate $DATAPATH \
+    --task translation \
+    --path $BASE_MODEL \
+    --dataset-impl mmap \
+    --source-lang en --target-lang ja \
+    --lenpen 0.8 --max-len-a 1.2 --max-len-b 10 \
+    --gen-subset test \
+    --beam 5 \
+    --batch-size 32 \
+    --remove-bpe \
+    --max-source-positions 1024 \
+    --max-target-positions 1024 \
+    --results-path $DIR/fairseq_out_test/ 
+
+grep '^H-' $DIR/fairseq_out_test/generate-test.txt > $DIR/fairseq_out_test/generate-test.tmp1
+rm $DIR/fairseq_out_test/generate-test.txt
+sort -t- -k2,2n $DIR/fairseq_out_test/generate-test.tmp1 > $DIR/fairseq_out_test/generate-test.tmp2
+cut -f 3- $DIR/fairseq_out_test/generate-test.tmp2 > $DIR/fairseq_out_test/generate-test.txt
+rm $DIR/fairseq_out_test/generate-test.tmp1 $DIR/fairseq_out_test/generate-test.tmp2
